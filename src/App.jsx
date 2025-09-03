de0c302b23e8a960a2200f1aae2b0afedafed475
JonatasPablo <jonathaspabllo@gmail.com>
2025-09-02 21:27:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+// PAGES
+import Home from './pages/Home'
+import About from './pages/About'
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
+//CONTEXTS
+import { AppContext } from './contexts/AppContext'
+
+//UTILS
+import ScrollToTop from './utils/ScrollTop'
+
+function App() {
+  const appContext = useContext(AppContext)
+
+  if(appContext.loading){
+    return <LoadingSpinner />
+  }
+
+  return (
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />}></Route> 
+        <Route path="/about" element={<About />}></Route> 
+        <Route path="/projects" element={<Projects />}></Route> 
+        <Route path="/contact" element={<Contact />}></Route> 
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
+
